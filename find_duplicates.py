724237a7342ee3c3a991c5994be38a373032067b
YuyaR <68813586+YuyaR@users.noreply.github.com>
2021-04-18 14:16:54 +0100

    Update find_duplicates.py

--- find_duplicates.py
+++ find_duplicates.py
@@ -8,7 +8,7 @@ Created on Sun Apr 18 12:51:25 2021
 
 import pandas as pd
 
-google_hotel = pd.read_csv("~/Desktop/python/Fiji/Google_Fijihotel.csv")
+google_hotel = pd.read_csv("PATH/TO/Google_Fijihotel.csv")
 
 unique_google = google_hotel.drop_duplicates(["rounded_latitude", "rounded_longitude"])
 
@@ -17,4 +17,4 @@ print(google_hotel[google_hotel.duplicated(["rounded_latitude", "rounded_longitu
 
 final_results = unique_google[['Name of Hotel', 'latitude', 'longitude', 'found on fiji gov website']]
 
-final_results.to_csv("~/Desktop/python/Fiji/Google_FijiHotel_final.csv", index=False, header=True)
+final_results.to_csv("PATH/TO/SAVE/FILE/Google_FijiHotel_final.csv", index=False, header=True)
